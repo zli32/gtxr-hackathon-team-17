d3e6eec4db7c8ff7bee74a2800426c0a84e873f1
Li, Zekun Lawrence <zli909@gatech.edu>
2023-04-08 19:04:58 -0400

    fixed camera focus

--- augmented_image_java/app/src/main/java/com/google/ar/core/examples/java/augmentedimage/AugmentedImageActivity.java
+++ augmented_image_java/app/src/main/java/com/google/ar/core/examples/java/augmentedimage/AugmentedImageActivity.java
@@ -420,6 +420,13 @@ public class AugmentedImageActivity extends AppCompatActivity implements GLSurfa
       messageSnackbarHelper.showError(this, "Could not setup augmented image database");
     }
     session.configure(config);
+    try {
+      session.resume();
+      session.pause();
+      session.resume();
+    } catch(Exception e) {
+
+    }
   }
 
   private void drawAugmentedImages(
